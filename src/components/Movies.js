11409e44d8cc798bfa582be7a600a3329f7e0aa5
Lara Sanders <larasanders@gmail.com>
2019-06-02 17:50:48 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
-   movies.map(movie => <Movie Title={movie.title} Genre={movie.genre} Time={movie.time}/>)
+  { movies.map(movie => <Movie Title={movie.title} Genre={movie.genre} Time={movie.time}/>) }
   return (
     <React.Fragment>
     <div>
       <h1> Movies Page </h1>
-      <h1>Movie Title={movie.title}</h1>
-
+      <h1>{movie.title}</h1>
       <ul>
-        <h2>Genre={movie.genre}</h2>
+        <h2>{movie.genre}</h2>
         <li>
-          <h2>Time={movie.time}</h2>
+          <h2>{movie.time}</h2>
         </li>
       </ul>
     </div>
